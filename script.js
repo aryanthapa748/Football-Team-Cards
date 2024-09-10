b8b753acd459a3561a82d74fa482bfe1ddc6b308
Aryan Thapa <aryanthapa748@gmail.com>
2024-09-10 10:37:27 -0400

    Adding filter method to filter player/s nickname which is not null

--- script.js
+++ script.js
@@ -196,6 +196,7 @@ playersDropdownList.addEventListener("change", (e) => {
   playerCards.innerHTML = '';
   switch (e.target.value) {
     case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
   }
 
 });
